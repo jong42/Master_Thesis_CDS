02aa83e4d34978e465561fdc476e508d80afdb2e
Philipp Lucas <philipp.lucas@uni-jena.de>
2018-11-13 09:47:12 +0100

    Implement marginalization method for fixed class

--- fixed_PyMC3_model.py
+++ fixed_PyMC3_model.py
@@ -17,24 +17,30 @@ class FixedProbabilisticModel(Model):
 
     def __init__(self, name):
         super().__init__(name)
+        self._model_structure = None
         self._model_params = None
 
     def _set_data(self, df, drop_silently, **kwargs):
+        # TODO: Is there a better place for decribing the whole model?
         self._set_data_mixed(df, drop_silently)
-        return ()
-
-    def _fit(self):
-        # Specify model
         basic_model = pm.Model()
         with basic_model:
             # describe prior distributions of model parameters.
             alpha = pm.Normal('alpha', mu=0, sd=10)
-            beta = pm.Normal('beta', mu=0, sd=10, shape=2)
+            beta1 = pm.Normal('beta1', mu=0, sd=10)
+            beta2 = pm.Normal('beta2', mu=0, sd=10)
             sigma = pm.HalfNormal('sigma', sd=1)
             # specify model for the output parameter.
-            mu = alpha + beta[0] * self.data['X1'] + beta[1] * self.data['X2']
+            mu = alpha + beta1 * self.data['X1'] + beta2 * self.data['X2']
             # likelihood of the observations. Observed stochastic variable
             Y_obs = pm.Normal('Y_obs', mu=mu, sd=sigma, observed=self.data['Y'])
+        self._model_structure = basic_model
+        return ()
+
+    def _fit(self):
+        # Specify model
+        basic_model = self._model_structure
+        with basic_model:
             # draw samples from the posterior
             trace = pm.sample(500)
         # getting the means of the parameters from the samples
@@ -43,26 +49,34 @@ class FixedProbabilisticModel(Model):
         return ()
 
     def _marginalizeout(self, keep, remove):
+        # Set data to comlete dataset for this method. change it back at the end of the method
+        data = self.data
+        self._set_data(df, 0)
         # Specify model
-        basic_model = pm.Model()
+        basic_model = self._model_structure
         with basic_model:
-            # describe prior distributions of model parameters.
-            alpha = pm.Normal('alpha', mu=0, sd=10)
-            beta = pm.Normal('beta', mu=0, sd=10, shape=2)
-            sigma = pm.HalfNormal('sigma', sd=1)
-            # specify model for the output parameter.
-            mu = alpha + beta[0] * self.data['X1'] + beta[1] * self.data['X2']
-            # likelihood of the observations. Observed stochastic variable
-            Y_obs = pm.Normal('Y_obs', mu=mu, sd=sigma, observed=self.data['Y'])
-            # sample only over the variables in keep
-            for index,name in enumerate(keep):
-                keep[index] = eval(name)
-            #keep = [eval(name) for name in keep] WHY DOES THIS NOT WORK? --> scope
-            sampling_properties = pm.backends.NDArray(vars=keep)
-            trace = pm.sample(500,trace=sampling_properties)
-        # getting the means of the parameters from the samples
-        parameter_means = pm.summary(trace).round(2).iloc[:,0]
-        self._model_params = parameter_means
+            trace = pm.sample(500)
+        # Get the values of the parameter samples
+        joint_sample = np.zeros(shape=(2000,8))
+        for i,var in enumerate(basic_model.unobserved_RVs):
+            joint_sample[:,i] = trace.get_values(var)
+        # Generate the data samples
+        X1_joint_sample = np.random.randn(2000)
+        X2_joint_sample = np.random.randn(2000) * 0.2
+        joint_sample[:,5] = X1_joint_sample
+        joint_sample[:,6] = X2_joint_sample
+        mu_joint_sample = joint_sample[:,0] + np.multiply(joint_sample[:,1], joint_sample[:,5].T) + np.multiply(joint_sample[:,2], joint_sample[:,6].T)
+        Y_joint_sample = np.random.normal(loc=mu_joint_sample, scale=joint_sample[:,4])
+        joint_sample[:,7] = Y_joint_sample
+         # Get the means of the the sampled dimensions
+        joint_sample_means = []
+        for col in range(0,joint_sample.shape[1]):
+            joint_sample_means = np.append(joint_sample_means,np.mean(joint_sample[:,col]))
+        # write only variables from keep to the model parameters
+        all_vars = {'alpha':0,'beta1':1,'beta2':2,'sigma_log':3,'sigma':4,'X1':5,'X2':6,'Y':7}
+        self._model_params = [joint_sample_means[index] for index in [all_vars[name] for name in keep]]
+        # change data back to previous selections
+        self.data = data
         return ()
 
     def _conditionout(self, keep, remove):
@@ -74,10 +88,14 @@ class FixedProbabilisticModel(Model):
                 alpha = 1
             else:
                 alpha = pm.Normal('alpha', mu=0, sd=10)
-            if ('beta' in remove):
-                beta = [1,1]
+            if ('beta1' in remove):
+                beta1 = 1
             else:
-                beta = pm.Normal('beta', mu=0, sd=10, shape=2)
+                beta1 = pm.Normal('beta1', mu=0, sd=10)
+            if ('beta2' in remove):
+                beta2 = 1
+            else:
+                beta2 = pm.Normal('beta2', mu=0, sd=10)
             if ('sigma' in remove):
                 sigma = 1
             else:
@@ -86,7 +104,7 @@ class FixedProbabilisticModel(Model):
             if ('mu' in remove):
                 mu = 1
             else:
-                mu = alpha + beta[0] * self.data['X1'] + beta[1] * self.data['X2']
+                mu = alpha + beta1 * self.data['X1'] + beta2 * self.data['X2']
             # likelihood of the observations. Observed stochastic variable
             Y_obs = pm.Normal('Y_obs', mu=mu, sd=sigma, observed=self.data['Y'])
             # draw samples from the posterior
@@ -99,26 +117,17 @@ class FixedProbabilisticModel(Model):
     def _density(self, x):
         # Mängel: Draw from prior oder posterior?
         #         Only 3 Parameter are selectable, keine Daten
-        #         Empir
+        #         Empirical density
 
         # Specify model
-        basic_model = pm.Model()
+        basic_model = self._model_structure
         with basic_model:
-            # describe prior distributions of model parameters.
-            alpha = pm.Normal('alpha', mu=0, sd=10)
-            beta = pm.Normal('beta', mu=0, sd=10, shape=2)
-            sigma = pm.HalfNormal('sigma', sd=1)
-            # specify model for the output parameter.
-            mu = alpha + beta[0] * self.data['X1'] + beta[1] * self.data['X2']
-            # likelihood of the observations. Observed stochastic variable
-            Y_obs = pm.Normal('Y_obs', mu=mu, sd=sigma, observed=self.data['Y'])
-            # draw samples from the posterior
-            vars = [alpha, beta, sigma]
+            vars = basic_model.unobserved_RVs
             sampling_properties = pm.backends.NDArray(vars=vars)
             trace = pm.sample(500,trace=sampling_properties)
             # Draw samples
-            samples = pd.DataFrame(columns=['alpha', 'beta', 'sigma'])
-            for var in ['alpha', 'beta', 'sigma']:
+            samples = pd.DataFrame(columns=vars)
+            for var in vars:
                 samples[var] = trace.get_values(var)
             # Calculate empirical density of point x on the samples
             density = data_op.density(samples, x)
@@ -126,16 +135,8 @@ class FixedProbabilisticModel(Model):
 
     def _sample(self):
         # Specify model
-        basic_model = pm.Model()
+        basic_model = self._model_structure
         with basic_model:
-            # describe prior distributions of model parameters.
-            alpha = pm.Normal('alpha', mu=0, sd=10)
-            beta = pm.Normal('beta', mu=0, sd=10, shape=2)
-            sigma = pm.HalfNormal('sigma', sd=1)
-            # specify model for the output parameter.
-            mu = alpha + beta[0] * self.data['X1'] + beta[1] * self.data['X2']
-            # likelihood of the observations. Observed stochastic variable
-            Y_obs = pm.Normal('Y_obs', mu=mu, sd=sigma, observed=self.data['Y'])
             trace = pm.sample(1)
             point = trace.point(0)
         return (point)
@@ -147,34 +148,37 @@ class FixedProbabilisticModel(Model):
 
 
 ### Generate data
-np.random.seed(123)
+#np.random.seed(123)
 # True parameter values
-alpha, sigma = 1, 1
-beta = [1, 2.5]
+#alpha, sigma = 1, 1
+#beta = [1, 2.5]
 
 # Size of dataset
-size = 100
+#size = 100
 
 # Predictor variable
-X1 = np.random.randn(size)
-X2 = np.random.randn(size) * 0.2
+#X1 = np.random.randn(size)
+#X2 = np.random.randn(size) * 0.2
 
 # Simulate outcome variable
-Y = alpha + beta[0]*X1 + beta[1]*X2 + np.random.randn(size)*sigma
+#Y = alpha + beta[0]*X1 + beta[1]*X2 + np.random.randn(size)*sigma
 # Create dataframe
-df = pd.DataFrame({'X1':X1, 'X2':X2, 'Y':Y})
+#df = pd.DataFrame({'X1':X1, 'X2':X2, 'Y':Y})
 ###
+
+df = pd.read_csv('fixed_PyMC3_example_data.csv')
 
 ### Set up and train model
 probabilistic_model_instance = FixedProbabilisticModel('probabilistic_model_instance')
 probabilistic_model_instance._set_data(df,0)
 #probabilistic_model_instance._fit()
-#probabilistic_model_instance._marginalizeout(keep = ['alpha','beta'], remove = ['sigma','mu','X1','X2','Y_obs'])
-#print(probabilistic_model_instance.fields)
-#print(probabilistic_model_instance._model_params)
-probabilistic_model_instance._conditionout(keep = ['alpha', 'beta'], remove = ['sigma','X1','X2','Y_obs'])
+probabilistic_model_instance._marginalizeout(keep = ['X1'], remove = ['alpha','beta1','beta2','sigma','mu','X1','Y_obs'])
+probabilistic_model_instance._marginalizeout(keep = ['alpha'], remove = ['alpha','beta','sigma','mu','X1','Y_obs'])
+print(probabilistic_model_instance.fields)
 print(probabilistic_model_instance._model_params)
-probabilistic_model_instance._conditionout(keep = ['sigma','X1','X2'], remove = ['alpha','beta','Y_obs'])
+probabilistic_model_instance._conditionout(keep = ['alpha', 'beta1'], remove = ['beta2','sigma','X1','X2','Y_obs'])
+print(probabilistic_model_instance._model_params)
+probabilistic_model_instance._conditionout(keep = ['sigma','X1','X2'], remove = ['alpha','beta1','beta2','Y_obs'])
 print(probabilistic_model_instance._model_params)
 ###
 print(probabilistic_model_instance._density([1,1,1]))
