ba0a7036f9192b9e1517c1d1112f47f702ed0dc9
Philipp Lucas <philipp.lucas@uni-jena.de>
2018-11-19 23:51:29 +0100

    Make methods more flexible

--- fixed_PyMC3_model.py
+++ fixed_PyMC3_model.py
@@ -25,47 +25,22 @@ class FixedProbabilisticModel(Model):
         self._update_all_field_derivatives()
         return ()
 
-    def _fit(self):
-        basic_model = pm.Model()
-        with basic_model:
-            # describe prior distributions of model parameters.
-            alpha = pm.Normal('alpha', mu=0, sd=10)
-            beta1 = pm.Normal('beta1', mu=0, sd=10)
-            beta2 = pm.Normal('beta2', mu=0, sd=10)
-            sigma = pm.HalfNormal('sigma', sd=1)
-            # specify model for the output parameter.
-            mu = alpha + beta1 * self.data['X1'] + beta2 * self.data['X2']
-            # likelihood of the observations. Observed stochastic variable
-            Y_obs = pm.Normal('Y_obs', mu=mu, sd=sigma, observed=self.data['Y'])
+    def _fit(self,model):
+        with model:
             # Draw samples
             trace = pm.sample(500)
-        self._model_structure = basic_model
-        self._joint_samples_alpha = trace.get_values('alpha')
-        self._joint_samples_beta1 = trace.get_values('beta1')
-        self._joint_samples_beta2 = trace.get_values('beta2')
-        self._joint_samples_sigma = trace.get_values('sigma')
-        self._joint_samples_X1 = np.random.randn(2000)
-        self._joint_samples_X2 = np.random.randn(2000) * 0.2
-        self._joint_samples_mu = self._joint_samples_alpha + np.multiply(self._joint_samples_beta1,self._joint_samples_X1.T) + np.multiply(self._joint_samples_beta2,self._joint_samples_X2.T)
-        self._joint_samples_Y = np.random.normal(loc=self._joint_samples_mu, scale=self._joint_samples_sigma)
+            self.samples = {}
+            for varname in trace.varnames:
+                self.samples[varname] = trace[varname]
         return ()
+
+# Achtung: _marginalizeout is currently only for parameters possible,
+# whereas _conditionout is only for data points possible
 
     def _marginalizeout(self, keep, remove):
         # Remove all variables in remove
-        if 'alpha' in remove:
-            self._joint_samples_alpha = None
-        if 'beta1' in remove:
-            self._joint_samples_beta1 = None
-        if 'beta2' in remove:
-            self._joint_samples_beta2 = None
-        if 'sigma' in remove:
-            self._joint_samples_sigma = None
-        if 'X1' in remove:
-            self._joint_samples_X1 = None
-        if 'X2' in remove:
-            self._joint_samples_X2 = None
-        if 'Y' in remove:
-            self._joint_samples_Y = None
+        for varname in remove:
+            self.samples[varname] = None
         return ()
 
     def _conditionout(self, keep, remove):
@@ -74,38 +49,58 @@ class FixedProbabilisticModel(Model):
         cond_domains = [field['domain'] for field in fields]
         return (cond_domains)
 
-    def _density(self, x):
+    # def _density(self, x):
+    #
+    #     X = np.array([self._joint_samples_alpha,self._joint_samples_beta1,
+    #                   self._joint_samples_beta2, self._joint_samples_sigma,
+    #                   self._joint_samples_X1, self._joint_samples_X2,
+    #                   self._joint_samples_mu]).T
+    #     kde = KernelDensity(kernel='gaussian', bandwidth=0.2).fit(X)
+    #     x = np.reshape(x,(1,len(x)))
+    #     logdensity = kde.score_samples(x)
+    #     return (np.exp(logdensity))
 
-        X = np.array([self._joint_samples_alpha,self._joint_samples_beta1,
-                      self._joint_samples_beta2, self._joint_samples_sigma,
-                      self._joint_samples_X1, self._joint_samples_X2,
-                      self._joint_samples_mu]).T
-        kde = KernelDensity(kernel='gaussian', bandwidth=0.2).fit(X)
-        x = np.reshape(x,(1,len(x)))
-        logdensity = kde.score_samples(x)
-        return (np.exp(logdensity))
+    # def _sample(self):
+    #     # Specify model
+    #     basic_model = self._model_structure
+    #     with basic_model:
+    #         trace = pm.sample(1)
+    #         point = trace.point(0)
+    #     return (point)
+    #
+    # def copy(self, name=None):
+    #     mycopy = self._defaultcopy(name)
+    #     mycopy._model_structure = self._model_structure
+    #     mycopy._joint_samples_alpha = self._joint_samples_alpha
+    #     mycopy._joint_samples_beta1 = self._joint_samples_beta1
+    #     mycopy._joint_samples_beta2 = self._joint_samples_beta2
+    #     mycopy._joint_samples_sigma = self._joint_samples_sigma
+    #     mycopy._joint_samples_X1 = self._joint_samples_X1
+    #     mycopy._joint_samples_X2 = self._joint_samples_X2
+    #     mycopy._joint_samples_mu = self._joint_samples_mu
+    #     mycopy.joint_samples_Y = self._joint_samples_Y
+    #
+    #     return (mycopy)
+    #
+    #
+    #
 
-    def _sample(self):
-        # Specify model
-        basic_model = self._model_structure
-        with basic_model:
-            trace = pm.sample(1)
-            point = trace.point(0)
-        return (point)
+data = pd.read_csv('fixed_PyMC3_example_data.csv')
 
-    def copy(self, name=None):
-        mycopy = self._defaultcopy(name)
-        mycopy._model_structure = self._model_structure
-        mycopy._joint_samples_alpha = self._joint_samples_alpha
-        mycopy._joint_samples_beta1 = self._joint_samples_beta1
-        mycopy._joint_samples_beta2 = self._joint_samples_beta2
-        mycopy._joint_samples_sigma = self._joint_samples_sigma
-        mycopy._joint_samples_X1 = self._joint_samples_X1
-        mycopy._joint_samples_X2 = self._joint_samples_X2
-        mycopy._joint_samples_mu = self._joint_samples_mu
-        mycopy.joint_samples_Y = self._joint_samples_Y
+basic_model = pm.Model()
+with basic_model:
+    # describe prior distributions of model parameters.
+    alpha = pm.Normal('alpha', mu=0, sd=10)
+    beta1 = pm.Normal('beta1', mu=0, sd=10)
+    beta2 = pm.Normal('beta2', mu=0, sd=10)
+    sigma = pm.HalfNormal('sigma', sd=1)
+    # specify model for the output parameter.
+    mu = alpha + beta1 * data['X1'] + beta2 * data['X2']
+    # likelihood of the observations. Observed stochastic variable
+    Y_obs = pm.Normal('Y_obs', mu=mu, sd=sigma, observed= data['Y'])
 
-        return (mycopy)
-
-
-
+mymodel = FixedProbabilisticModel('mymodel')
+mymodel._set_data(data,1)
+mymodel._fit(basic_model)
+mymodel._marginalizeout(['hurz'],['sigma'])
+mymodel._conditionout(['hurz'],['X1'])
